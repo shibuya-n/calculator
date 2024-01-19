497d73a5a17e0571717ee549964614151a958c76
shibuya-n <a.duong2006@gmail.com>
2024-01-19 15:11:36 -0800

    init commit

--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame {
 
     public GUI(){
         super("CALCULATOR");
-        Container c = get
+        Container c = getContentPane();
+        c.setLayout(new GridLayout(3,1));
+
+        JPanel y = new JPanel();
+        y.setLayout(new FlowLayout());
+
+        JPanel z = new JPanel();
+        z.setLayout(new FlowLayout());
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GUI window = new GUI();
+        window.setVisible(true);
+        window.setBounds(100,100,300,100);
+        window.setResizable(true);
+
+        y.add(new JTextField("Derivative Calculator"));
+        y.add()
+
     }
 }
