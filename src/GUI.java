deeab509feeffea59b48200b87a602c2191314cb
shibuya-n <a.duong2006@gmail.com>
2024-01-19 14:55:46 -0800

    init commit

---
+++ src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI(){
+        super("CALCULATOR");
+        Container c = get
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
